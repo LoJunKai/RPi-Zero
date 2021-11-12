73df19dc764aeb776f84b92910faa7b8a9628eb5
Lo Jun Kai <lojunkai98@gmail.com>
2021-11-12 16:11:04 +0800

    Minor merge refinements

--- testing/config.py
+++ testing/config.py
@@ -1,6 +1,14 @@
 # Configuration file to keep track of shared variables
 
+# Test files
+TEST_FILE1 = "./cMsg.json"
+TEST_FILE2 = "./sMsg.json"
+
+
+# gRPC
+
+
+# MQTT
 MQTT_BROKER_IP = "192.168.1.94"  # Fill this up with the mqtt broker hostname
 
 TEST_TOPIC1 = "testing/bandwidth_test"
-TEST_FILE1 = "/filepath/to/textfile"
