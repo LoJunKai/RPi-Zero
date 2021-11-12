fd6246f97f9421270724c8f1de939689fe0eb52c
Lo Jun Kai <lojunkai98@gmail.com>
2021-11-12 21:13:35 +0800

    Add Profiler code
    Currently not working:
    Problem with timer.
    Problem with logger.
    Problem with function arguments.

--- config.py
+++
@@ -1,6 +0,0 @@
-# Configuration file to keep track of shared variables
-
-MQTT_BROKER_IP = "192.168.1.94"  # Fill this up with the mqtt broker hostname
-
-TEST_TOPIC1 = "testing/bandwidth_test"
-TEST_FILE1 = "/filepath/to/textfile"
