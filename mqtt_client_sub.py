6edebc6e06d46711dd2438df688303bd2fcc6b64
yyx543 <yyx543@gmail.com>
2021-11-15 19:43:05 +0800

    client sub

---
+++ mqtt_client_sub.py
@@ -0,0 +1,27 @@
+import paho.mqtt.client as subscriber
+import time
+
+from config import *
+
+def on_message(client, userdata, message):
+    print(f"message: {message.payload}")
+    print(f"topic: {message.topic}")
+    print(f"qos: {message.qos}")
+    print(f"retain flag: {message.retain}")
+
+print("Creating Client")
+client = subscriber.Client("sub1")
+client.on_message = on_message
+
+print(f"Connecting to {MQTT_BROKER_IP}")
+client.connect(MQTT_BROKER_IP)
+
+#loop start
+client.loop_start()
+
+print(f"Subscribing to {TEST_TOPIC1}")
+client.subscribe(TEST_TOPIC1)
+
+time.sleep(10)
+
+client.loop_stop()
